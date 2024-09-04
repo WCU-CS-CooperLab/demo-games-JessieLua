c58f0e12ca9a895311048b400683eedb249b5333
JessieLua <89940682+JessieLua@users.noreply.github.com>
2024-09-03 22:19:16 -0400

    player scene

---
+++ coin-dash/player.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var speed = 350
+var velocity = Vector2.ZERO
+var screensize = Vector2(480,720)
+
+func _process(delta):
+	velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	position += velocity * speed * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
